231118ee9ca8cbaa627c05d39fe5108bef67d3f5
fuyishun <452625491@qq.com>
2016-12-23 17:48:58 +0800

    修改了MainActivity

--- app/src/main/java/com/fys/test/MainActivity.java
+++ app/src/main/java/com/fys/test/MainActivity.java
@@ -2,9 +2,12 @@ package com.fys.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
